e8f543ba966a42e923afcd47b881e0dfb1ba9274
Jonathan Sturges <jsturges@redhat.com>
2026-09-20 22:28:15 -0400

    fix device name in platform.sh

--- target/linux/mediatek/filogic/base-files/lib/upgrade/platform.sh
+++ target/linux/mediatek/filogic/base-files/lib/upgrade/platform.sh
@@ -277,7 +277,7 @@ platform_do_upgrade() {
 		;;
 	asus,rt-ax52|\
 	asus,rt-ax57m|\
-	asus,rt-ax57_go|\
+	asus,rt-ax57go|\
 	asus,rt-ax59u|\
 	asus,tuf-ax4200|\
 	asus,tuf-ax4200q|\
@@ -545,7 +545,7 @@ platform_pre_upgrade() {
 	case "$board" in
 	asus,rt-ax52|\
 	asus,rt-ax57m|\
-	asus,rt-ax57_go|\
+	asus,rt-ax57go|\
 	asus,rt-ax59u|\
 	asus,tuf-ax4200|\
 	asus,tuf-ax4200q|\
